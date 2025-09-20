4300355cb0cba4e64576cdbc2f5266ff99a61f6f
Bruno Garcia <bkgarcia98@gmail.com>
2025-09-19 22:55:08 -0300

    feat: inicia projetos com configurações básicas.

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import '../../styles/global.css';
+import styles from './styles.module.css';
+
+export function Dashboard () {
+    return <>
+        <div>
+            <h1 className= {styles.header}>Bloco de notas</h1>
+        </div>
+    </>
+}
