e5132cb31168cd496211c590afbb97c0e33337ce
Bruno Garcia <bkgarcia98@gmail.com>
2025-09-30 21:42:57 -0300

    feat(NotesForm): cria função handle para registrar anotação.

--- src/components/NotesForm/index.tsx
+++ src/components/NotesForm/index.tsx
@@ -1,12 +1,33 @@
+import { useState } from 'react'
 import styles from './styles.module.css'
+import type { NotesModel } from '../../Models/NotesModel'
 
 export function NotesForm() {
+
+    const [state, setState] = useState<NotesModel>({
+        id: '',
+        title: '',
+        registerDate: Date.now(),
+        content: ''
+    })
+
+    function handleSaveNote(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        console.log(state)
+    }
+
     return <>
-        <div >
-            <form className = {styles.area}>
-                <input></input>
-                <input></input>
-                <button>Registrar Anotação</button>
+        <div>
+            <form onSubmit={handleSaveNote} className = {styles.area}>
+                <input
+                value = {state?.title}
+                onChange={e => setState({...state, title: e.target.value})}></input>
+
+                <input
+                value = {state?.content}
+                onChange={e => setState({...state, content: e.target.value})}></input>
+
+                <button type='submit'>Registrar Anotação</button>
             </form>
         </div>
     </>
